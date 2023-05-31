17b3ca035343240a32facdcd9ba3ab9ddbad91a3
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-31 17:38:50 +0700

    Membuat objek pelajar dan mencetak perkenalan siswa

--- Program Multiwaris/Program Multiwaris.cpp
+++ Program Multiwaris/Program Multiwaris.cpp
@@ -43,6 +43,13 @@ public:
 	}
 	string perkenalan() {
 		return "Hallo, nama saya " + nama + " dengan jenis kelamin "
-			+ jeniskelamin + " dari sekolah " + sekolah + "n\n";
+			+ jeniskelamin + " dari sekolah " + sekolah + "\n\n";
 	}
 };
+
+int main() {
+	pelajar siswa("andi laksono", "laki-laki", "SMAN 1 BANTUL");
+	cout << siswa.perkenalan();
+
+	return 0;
+}
