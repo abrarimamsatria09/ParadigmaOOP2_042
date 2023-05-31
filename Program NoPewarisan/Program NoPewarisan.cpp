eb61e7e9d0c84fd0b3d8f4aaa51022f952d4b292
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-31 16:59:41 +0700

    Tambahkan DeriveClass dengan fungsi pengatur dan fungsi utama dengan instance DeriveClass dan pemanggilan fungsi

--- Program NoPewarisan/Program NoPewarisan.cpp
+++ Program NoPewarisan/Program NoPewarisan.cpp
@@ -7,4 +7,15 @@ public:
 		cout << "Hallo saya function dari base class";
 	}
 };
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya function dari derived class";
+	}
+};
+int main() {
+	derivedClass a;
+	a.perkenalan();
 
+	return 0;
+}
